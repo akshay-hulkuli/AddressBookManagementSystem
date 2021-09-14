868ffac1475de26b04983cbb1cbc004ed3c6387c
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-14 19:51:30 +0530

    ADDED: class that provides main method

---
+++ lib/src/main/java/com/bridgelabz/addressbook/AddressBookSystem.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookSystem {
+	public static void main(String[] args) {
+		System.out.println("****** Welcome to address book program ! ******");
+		
+		AddressBook addressBook = new AddressBook();
+		addressBook.addPerson();
+		
+	}
+}
