d62e2b293cd1532decc802bed4bd68056aff97ae
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-19 22:38:23 +0530

    ADDED: menu options to call writeFile and readFile methods

--- lib/src/main/java/com/bridgelabz/addressbook/AddressBookSystem.java
+++ lib/src/main/java/com/bridgelabz/addressbook/AddressBookSystem.java
@@ -19,7 +19,7 @@ public class AddressBookSystem {
 		int option = 0;
 		boolean exit = true;
 		while(exit) {
-			System.out.println("Select option 1: add user.  2: edit existing user.  3: display all users 4:Delete contact. 5: sortby name 6:Switch Address Book");
+			System.out.println("Select option 1: add user.  2: edit existing user.  3: display all users 4:Delete contact. 5: sortby name 6:write file 7:read file  8:Switch Address Book");
 			option  = sc.nextInt();
 			switch(option) {
 				case 1 :
@@ -59,7 +59,12 @@ public class AddressBookSystem {
 							System.out.println("worng entry");
 					}
 					break;
-					
+				case 6:
+					addressBook.writeDataToFile();
+					break;
+				case 7:
+					addressBook.readDataFromFile();
+					break;
 				default:
 					exit = false;
 					
