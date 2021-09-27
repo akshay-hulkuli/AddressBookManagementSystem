19ee4ca41cca8c8a826defaeecb2eb38a475d794
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-27 22:23:15 +0530

    ADDED: class to define custom exception

---
+++ lib/src/main/java/com/bridgelabz/addressbook/AddressBookException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbook;
+
+
+public class AddressBookException extends RuntimeException  {
+	enum ExceptionType {
+        FAILED_TO_CONNECT, CANNOT_EXECUTE_QUERY, UPDATE_FAILED
+    }
+    ExceptionType exceptionType;
+
+    public AddressBookException(ExceptionType exceptionType, String message) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+}
