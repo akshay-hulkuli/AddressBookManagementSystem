cb38be7abb39eef03d9a664716921f5b915a6413
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-30 19:40:57 +0530

    ADDED: pojo class to hold address of a contact

---
+++ lib/src/main/java/com/bridgelabz/addressbook/Address.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.addressbook;
+
+public class Address {
+	private int contactId;
+	private String address;
+	private String state;
+	private String city;
+	private int pinCode;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(int pinCode) {
+		this.pinCode = pinCode;
+	}
+	public int getContactId() {
+		return contactId;
+	}
+	public void setContactId(int contactId) {
+		this.contactId = contactId;
+	}
+	
+	
+}
