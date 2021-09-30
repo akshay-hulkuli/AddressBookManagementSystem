c59519b168523fbbabd5c9022d969be1468a5aae
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-30 19:42:02 +0530

    ADDED: pojo class that holds contact information

---
+++ lib/src/main/java/com/bridgelabz/addressbook/Contacts.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.addressbook;
+
+import java.time.LocalDate;
+
+public class Contacts {
+	private int contactId;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String email;
+	private LocalDate dateAdded;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public LocalDate getDateAdded() {
+		return dateAdded;
+	}
+	public void setDateAdded(LocalDate date_added) {
+		this.dateAdded = date_added;
+	}
+	public int getContactId() {
+		return contactId;
+	}
+	public void setContactId(int contactId) {
+		this.contactId = contactId;
+	}
+	
+	
+}
