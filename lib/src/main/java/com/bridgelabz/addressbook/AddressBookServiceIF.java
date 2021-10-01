bde3d2ea0adc0656053e3036a3fa67a6272d2932
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-01 16:46:07 +0530

    ADDED: interface that defines tasks performed by addressBook

---
+++ lib/src/main/java/com/bridgelabz/addressbook/AddressBookServiceIF.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.addressbook;
+
+public interface AddressBookServiceIF {
+	public void addPerson(PersonDetails person , IOServiceEnum type);
+	public long readData(IOServiceEnum type);
+	public void searchByCity(String city,String firstName);
+	public void searchByState(String state, String firstName);
+	public void personsInCity(String city);
+	public void personsInState(String State);
+	public int countByCity(String city, IOServiceEnum type);
+	public int countByState(String state,IOServiceEnum type);
+	public void editPerson(String name);
+	public void deletePerson(String name);
+	public  void sortByFirstName();
+	public  void sortByZip();
+	public  void sortByCity();
+	public  void sortByState();
+}
