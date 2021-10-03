4c1d7364e08d1c71d7609906f70bd7d99f9168de
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-03 10:20:31 +0530

    REFACTORED: txt, csv,json and database test cases are moved to separate classes

--- lib/src/test/java/com/bridgelabz/addressbook/AddressBookTest.java
+++ lib/src/test/java/com/bridgelabz/addressbook/AddressBookTest.java
@@ -1,9 +1,5 @@
 package com.bridgelabz.addressbook;
 
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -11,14 +7,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import com.google.gson.Gson;
 
 public class AddressBookTest {
 	PersonDetails person1;
@@ -107,152 +98,5 @@ public class AddressBookTest {
 		Assert.assertEquals(2,size);
 	}
 	
-	@Test
-	public void givenAContact_WhenAddedToCSVFile_ShouldReturnCorrectSize() {
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();	
-		addressBook.addPerson(person1, IOServiceEnum.CSV_IO);
-		addressBook.addPerson(person2, IOServiceEnum.CSV_IO);
-		long size = 0;
-		try {
-			size = Files.lines(Paths.get("AddressBook-file.csv")).count();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		Assert.assertEquals(2,size-1);
-	}
-	
-	@Test
-	public void whenCalled_ReadFromCSVMethod_ShouldPrintFile() {
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();	
-		addressBook.addPerson(person1, IOServiceEnum.CSV_IO);
-		addressBook.addPerson(person2, IOServiceEnum.CSV_IO);
-		long size  = addressBook.readData(IOServiceEnum.CSV_IO);
-		Assert.assertEquals(2,size);
-	}
-	
-	@Test
-	public void givenAContact_WhenAddedToJSONFile_ShouldReturnCorrectSize() {
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();	
-		addressBook.addPerson(person1, IOServiceEnum.JSON_IO);
-		addressBook.addPerson(person2, IOServiceEnum.JSON_IO);
-		long count =0;
-		try {
-			Gson gson = new Gson();
-			BufferedReader br = new BufferedReader(new FileReader("AddressBook-file.json"));
-			PersonDetails[] usrObj = gson.fromJson(br, PersonDetails[].class);
-			List<PersonDetails> csvUSerList = Arrays.asList(usrObj);
-			count = csvUSerList.size();
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
-		Assert.assertEquals(2,count);
-	}
-	
-	@Test
-	public void whenCalled_ReadFromJsonMethod_ShouldPrintFile() {
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();	
-		addressBook.addPerson(person1, IOServiceEnum.JSON_IO);
-		addressBook.addPerson(person2, IOServiceEnum.JSON_IO);
-		long size  = addressBook.readData(IOServiceEnum.JSON_IO);
-		Assert.assertEquals(2,size);
-	}
-	
-	@Test
-	public void givenAddressBookInDB_WhenRetrived_ShouldReturnCount()
-	{
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();
-		long count  = addressBook.readData(IOServiceEnum.DB_IO);
-		Assert.assertEquals(6, count);
-	}
-	
-	@Test
-	public void givenAContact_WhenInserted_ShouldGetUpdatedSize()
-	{
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();
-		long initialSize  = addressBook.readData(IOServiceEnum.DB_IO);
-		addressBook.addPerson(person1, IOServiceEnum.DB_IO);
-		long updatedSize = addressBook.readData(IOServiceEnum.DB_IO);
-		Assert.assertEquals(initialSize+1, updatedSize);
-	}
-	
-	@Test
-	public void givenAAddressBookName_WhenInserted_ShouldReturnOne()
-	{
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();
-		int result = addressBook.insertAddressBook("address_book3");
-		Assert.assertTrue(result==1);
-	}
-	
-	@Test
-	public void givenContactIdAndPhonenumber_WhenUpdated_shouldReturnOne()
-	{
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();
-		addressBook.readData(IOServiceEnum.DB_IO);
-		addressBook.updatePhonenumberOfContact("4568561236", 1);
-		boolean result = addressBook.checkAddressBookInsyncWithDB(1);
-		Assert.assertTrue(result);
-	}
-
-	@Test
-	public void givenDateRange_FindContactsAddedInThatRange_ShouldReturnCount() {
-		AddressBookServiceImpl addressBookService = new AddressBookServiceImpl();
-		String date1 = "2019-01-01";
-		String date2 = "2020-09-09";
-		List<AddressBookData> contactList = addressBookService.getEmployeeInADateRange(date1,date2);
-		System.out.println(contactList);
-		sum =0;
-		contactList.stream().forEach(addressBook -> {
-			addressBook.getContactList().stream().forEach(contact -> sum += contact==null?0:1);
-		});
-		Assert.assertEquals(3, sum);
-	}
-	
-	@Test
-	public void givenCity_FindContactsInThatCity_ShouldReturnCount() {
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();
-		HashMap<String,ArrayList<String>> contactsByAddressBook = addressBook.getContactsByCity("Bengaluru");
-		System.out.println(contactsByAddressBook);
-		Assert.assertEquals(2, contactsByAddressBook.get("address_book1").size());
-	}
-	
-	@Test
-	public void givenState_FindContactsInThatState_ShouldReturnCount() {
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();
-		HashMap<String,ArrayList<String>> contactsByAddressBook = addressBook.getContactsByState("Maharashtra");
-		System.out.println(contactsByAddressBook);
-		Assert.assertEquals(2, (contactsByAddressBook.get("address_book1").size()+contactsByAddressBook.get("address_book2").size()));
-	}
-	
-	@Test
-	public void givenAWrongQuery_WhenExecuted_ShouldThrowCustomException()
-	{
-		DBServiceProvider databaseIO = DBServiceProvider.getInstance();
-		String sql = "SELECT * FROM random_table";
-		try {
-			ExpectedException exceptionRule = ExpectedException.none();
-			exceptionRule.expect(AddressBookException.class);
-			databaseIO.demoQuery(sql);
-		}
-		catch( AddressBookException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	@Test
-	public void givenAWorngContactData_WhenInserted_ShouldThrowCustomException()
-	{
-		AddressBookServiceImpl addressBook = new AddressBookServiceImpl();
-		try {
-			ExpectedException exceptionRule = ExpectedException.none();
-			exceptionRule.expect(AddressBookException.class);
-			addressBook.addPerson(person2, IOServiceEnum.DB_IO);
-		}
-		catch( AddressBookException e) {
-			e.printStackTrace();
-		}
-	}
 	
 }
