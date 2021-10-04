82496c41b62cbb9d0c78f620cbb95653353dc5e4
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-04 19:12:47 +0530

    ADDED: dto classes to transfer data

---
+++ lib/src/main/java/com/bridgelabz/dto/Address.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.dto;
+
+public class Address {
+	private String address;
+	private String state;
+	private String city;
+	private int pinCode;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(int pinCode) {
+		this.pinCode = pinCode;
+	}
+}
